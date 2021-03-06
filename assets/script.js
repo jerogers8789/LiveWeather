adc76c8ec298c967eceea50ed1d944889675d0f8
Jeremiah Rogers <je.rogers8789@gmail.com>
2021-03-05 23:13:55 -0600

    added base js, currently nonfunctiong

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,5 @@
-fetch('http://example.com/movies.json')
+var citySearch = document.getElementById('#city')
+
+$('#search').onclick = fetch('https://api.openweathermap.org/data/2.5/weather?q={$(#city).value}&appid={a97dec38647ca1a3f70e8d72776c1f18}')
   .then(response => response.json())
   .then(data => console.log(data));
