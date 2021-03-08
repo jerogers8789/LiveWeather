7c511a2bc1f2fc0b11707ff478aeda58c25246d8
Jeremiah Rogers <je.rogers8789@gmail.com>
2021-03-08 15:31:23 -0600

    .

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,8 @@
-var citySearch = document.getElementById('#city')
+var citySearch = $('#city')
 
-$('#search').onclick = fetch('https://api.openweathermap.org/data/2.5/weather?q={$(#city).value}&appid={a97dec38647ca1a3f70e8d72776c1f18}')
-  .then(response => response.json())
-  .then(data => console.log(data));
+function searchCity() {
+fetch('https://api.openweathermap.org/data/2.5/weather?q'+ citySearch + '&appid' + a97dec38647ca1a3f70e8d72776c1f18)
+};
+  
+
+$('#search').onclick = searchCity
