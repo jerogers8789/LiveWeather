c55c44b0522381b6f6760451257db61700fd9e8d
JER.8789 <je.rogers8789@gmail.com>
2021-03-08 19:11:32 -0600

    .

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,10 @@
-var citySearch = $('#city').val()
-
+var citySearch = $('.city-name').attr('value');
+//var citySearch = 'San Antonio'
 function searchCity() {
-fetch('https://api.openweathermap.org/data/2.5/weather?q'+ citySearch + '&appid' + a97dec38647ca1a3f70e8d72776c1f18)
-    .then(response => response.json())
+fetch('http://api.openweathermap.org/data/2.5/weather?q='+ citySearch + '&appid=' + 'a97dec38647ca1a3f70e8d72776c1f18')
+    .then(response => {return response.json()})
     .then(data => console.log(data));
 };
   
 
-$('#search').onclick = searchCity
+$('#search').click(function (){searchCity})
